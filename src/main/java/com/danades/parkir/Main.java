605f00979fffc4bd53066c0527e61256f30be1a2
amridanades <amridanades@gmail.com>
2018-10-02 15:33:16 +0700

    class main

--- src/main/java/com/danades/parkir/Main.java
+++ src/main/java/com/danades/parkir/Main.java
@@ -5,7 +5,11 @@
  */
 package com.danades.parkir;
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,6 +18,32 @@ package com.danades.parkir;
 public class Main {
     public static void main(String[] args) {
         
-       
+        boolean running = true;
+        String answer = "n";
+        
+        while(running){
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader input = new BufferedReader(isr);
+
+            Parkir p = new Parkir();
+            try {
+                System.out.print("Jenis Kendaraan : ");
+                p.kendaraan = input.readLine();
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            p.CekLot();
+             System.out.println("----------------------------------------------------------------------");
+            System.out.println("Apakah anda ingin keluar? ");
+            System.out.print("[Y/N] : ");
+            try {
+                answer = input.readLine();
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            if( answer.equalsIgnoreCase("Y") ){
+                running = false;
+            }
+        }
     }
 }
