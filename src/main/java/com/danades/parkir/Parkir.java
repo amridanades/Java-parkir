5379e9a08ad7f52d4c5a83ebe3227639ecbd5101
amridanades <amridanades@gmail.com>
2018-10-02 15:27:08 +0700

    class parkir

---
+++ src/main/java/com/danades/parkir/Parkir.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.danades.parkir;
+
+/**
+ *
+ * @author Amri Danades
+ */
+public class Parkir {
+    
+    String kendaraan;
+    String[] area = {
+        "- Area parkir motor",
+        "- Area parkir mobil",
+        "- Area parkir bus"
+    };
+    
+    void CekLot(){   
+        System.out.println("\nLot parkir yang diperbolehkan :");
+        if (kendaraan.equalsIgnoreCase("motor"))
+             System.out.printf(area[0]+ "\n" + area[1] + "\n" + area[2] + "\n");
+        else if (kendaraan.equalsIgnoreCase("mobil"))
+            System.out.printf(area[1]+ "\n" + area[2] + "\n");
+        else if (kendaraan.equalsIgnoreCase("bus"))
+            System.out.printf(area[2] + "\n");
+        else System.out.printf("Kendaraan tidak sesuai \n");
+    }
+}
